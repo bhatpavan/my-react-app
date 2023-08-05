708e8f652b62df327c43266b1677182a13254baf
Pavan <pavanbhat0596@gmail.com>
2023-08-05 14:26:34 +0530

    Basic Fundemental

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
-import { Card } from "./components/Card";
-import { Navbar } from "./components/Navbar";
-import { Welcome } from "./components/Welcome";
 import "./App.css"
 
 function App() {
-  let cardelements = [{
-    imgUrl: require("./assets/image 12.png"),
-    country: "USA",
-    noOfRatings: 12,
-    rating: 5.0,
-    openSpots: 20,
-  },
-  {
-    imgUrl: require("./assets/image 12.png"),
-    country: "USA",
-    noOfRatings: 12,
-    rating: 5.0,
-    openSpots: 0,
-  },
-  {
-    imgUrl: require("./assets/image 12.png"),
-    country: "IND",
-    noOfRatings: 12,
-    rating: 3.0,
-    openSpots: 20,
-  }]
   return (
-    <>
-      <Navbar></Navbar>
-      <Welcome></Welcome>
-      <div className="app-cards">
-        {cardelements.map(x =>
-          <Card
-            imgUrl={x.imgUrl}
-            country={x.country}
-            noOfRatings={x.noOfRatings}
-            rating={x.rating}
-            openSpots={x.openSpots}
-            >
-          </Card>)}
-      </div>
-
-    </>
+    <h1>Hello</h1>
   )
 }
 
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import "./Navbar.css"
-
-export function Navbar(){
-    return (
-        <nav>
-        <img className="navbar-img" src={require("../assets/airbnb 1.png")}></img>
-        </nav>
-    )
-}
